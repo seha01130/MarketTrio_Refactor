b3002ef78e1ae84d5b85c7f4cb43aff41ae9b5dd
seha01130 <seha01130@gmail.com>
2026-02-23 12:34:39 +0900

    Fix test configuration to reference correct Spring Boot application class

--- marketTrio/src/test/java/com/example/jpetstore/JpetstoreBootApplicationTests.java
+++ marketTrio/src/test/java/com/example/jpetstore/JpetstoreBootApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.jpetstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.marketTrio.MarketTrioBootApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = MarketTrioBootApplication.class)
 class ReadinglistApplicationTests {
 
 	@Test
