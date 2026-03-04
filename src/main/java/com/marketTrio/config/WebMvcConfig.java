cf804e7d00d6f184bb9a6fa8dc153e329197e2d0
seha01130 <seha01130@gmail.com>
2026-03-05 00:56:43 +0900

    fix: 경매 디테일 페이지의 이미지 안보이는 오류 해결
    - 맥/리눅스 등 환경에 관계없이 프로퍼티만 변경하면 업로드 경로를 일관되게 관리할 수 있도록 구성

--- src/main/java/com/marketTrio/config/WebMvcConfig.java
+++ src/main/java/com/marketTrio/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.marketTrio.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
@@ -11,6 +12,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+	@Value("${file.upload-dir}")
+	private String uploadDir;
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("thyme/main");
@@ -18,7 +22,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/upload/**").addResourceLocations("file:C:/absolute/path/to/upload/");
+		registry.addResourceHandler("/upload/**")
+				.addResourceLocations("file:" + uploadDir + "/");
 	}
 
 	@Bean
